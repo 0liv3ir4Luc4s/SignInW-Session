6c5814d155eb22109b0b439a47e51558b7604b81
Lucas Oliveira <0liv3ir4.luc4s@gmail.com>
2021-12-18 21:03:37 -0300

    refactor: Compile login logic into less functions

--- app/back/controllers/UserController.php
+++ app/back/controllers/UserController.php
@@ -1,24 +1,25 @@
 <?php
     require_once("model/User.php");
-    require_once("Connection.php");
+    require_once("controllers/Connection.php");
     class UserController 
     {
         function selecionarUm($email)
         {
             try {
-                $con = new Connection("config.ini");
-                $comand = $thisj->con->getConnection()->prepare("SELECT from user WHERE email=$email"); 
+                $con = new Connection("controllers/config.ini");
+                $comand = $con->getPDO()->prepare("SELECT * FROM user WHERE email='$email';"); 
                 if ($comand->execute()) {
                     $result = $comand->fetchAll(PDO::FETCH_CLASS, "User");
                     $user = new User();
                     $user->setEmail($result[0]->getEmail());
-                    $user->setPassword($result[0]->getPassword());
+                    $user->setSenha($result[0]->getSenha());
                 }
             } catch (PDOException $PDOex) {
-                echo("Erro no banco de dados".$PDOex->getMessage());
+                echo("Erro no banco de dados {$PDOex->getMessage()}");
             } catch (Exception $ex) {
-                echo("Erro geral".$ex->getMessage());
+                echo("Erro geral {$ex->getMessage()}");
             } finally {
+                $con->closeConnection();
                 return $user;
             }
         }
@@ -26,6 +27,6 @@
         public function login($user)
         {
             $registeredUser = $this->selecionarUm($user->getEmail());
-            return $registeredUser->login($user->getEmail(), $user->getPassword());
+            return $registeredUser->login($user->getEmail(), $user->getSenha());
         }
     }
