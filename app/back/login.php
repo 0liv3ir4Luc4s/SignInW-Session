5d73645e844dc6a1a7f48bf75aee6e564493d865
Lucas Oliveira <0liv3ir4.luc4s@gmail.com>
2021-12-18 23:48:48 -0300

    feat: Add support to sessions

--- app/back/login.php
+++ app/back/login.php
@@ -1,13 +1,19 @@
 <?php
     require_once("controllers/UserController.php");
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    session_start();
+    $email = $_POST['email'] ?? $_SESSION['email'];
+    $password = $_POST['password'] ?? $_SESSION['password'];
     if (!empty($email) && !empty($password)) {
         $user = new User();
         $user->setEmail($email);
         $user->setSenha($password);
         $userController = new UserController();
         if ($userController->login($user)) {
+            if ($_POST['remember-password'] == "on") {
+                session_start();
+                $_SESSION['email'] = $email;
+                $_SESSION['password'] = $password;
+            }
             header("Location: http://lucaso.ntectreinamentos.com.br/proj3/success.html");
         } else {
             header("Location: http://lucaso.ntectreinamentos.com.br/proj3/error.html");
