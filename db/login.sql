13339e5e597d9c3d36c5648c492880618c45015b
0liv3ir4Luc4s <87442959+0liv3ir4Luc4s@users.noreply.github.com>
2021-12-23 19:25:16 -0300

    Add PRIMARY KEY constraint

--- db/login.sql
+++ db/login.sql
@@ -2,5 +2,6 @@ CREATE SCHEMA IF NOT EXISTS login;
 
 CREATE TABLE IF NOT EXISTS login.user(
     email VARCHAR(254) NOT NULL,
-    senha VARCHAR(255) NOT NULL
-)
+    senha VARCHAR(255) NOT NULL,
+    PRIMARY KEY(email)
+);
